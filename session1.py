1fd0ffed0db1dc19d472208e24918f02b85cd30c
Oliver Dressler <oliver.dressler@chem.ethz.ch>
2013-10-17 13:33:41 +0200

    Some changes added roundup...

--- session1.py
+++ session1.py
@@ -34,8 +34,12 @@ f = e % 7 # Modulo: Remainder when divided by 7
 
 # Exercise 6: Numbers default to floats if needed
 a = 4
-b = 13.0
+b = 13.
 # print 'EX6:', a+b, a/b
+
+# Exercise 7: Integers are rounded down
+a = 11
+# print 'EX7:', a/2, a/2.0
 
 
 
@@ -43,13 +47,13 @@ b = 13.0
 # Boolean
 ####
 
-# Exercise 7: Getting to know booleans
+# Exercise 8: Getting to know booleans
 a = 4
 b = a
 c = ( a == b )
 d = ( 4 == 4.0 )
 e = ( c != d )
-# print 'EX7:', c, d, e
+# print 'EX8:', c, d, e
 
 
 
@@ -57,35 +61,35 @@ e = ( c != d )
 # String
 ####
 
-# Exercise 8: Basic strings and string addition
+# Exercise 9: Basic strings and string addition
 a = 'i am a string'
 b = ', so what?'
 c = a + b
-# print 'EX8:', c
+# print 'EX9:', c
 
-# Exercise: Escaping characters
-# print 'EX6:', 'i\'m a string', '\nme too\n\t!!!!!!!!!!!!!!!!!!'
+# Exercise 10: Escaping characters
+# print 'EX10:', 'i\'m a string', '\nme too\n\t!!!!!!!!!!!!!!!!!!'
 
-# Exercise 9: Multiline strings
+# Exercise 11: Multiline strings
 multiline_string = '''I am on the first line.
 I\'m on the second line.
 	I was last...'''
-# print 'EX9:', multiline_string
+# print 'EX11:', multiline_string
 
-# Exercise 10: String slices (parts)
+# Exercise 12: String slices (parts)
 a = '12345'
 b = a[0]
 c = a[1:5]
-# print 'EX10:', b, c, c + b, a[1:5] + a[0]
+# print 'EX12:', b, c, c + b, a[1:5] + a[0]
 
-# Exercise 11: Length of a string
+# Exercise 13: Length of a string
 len_a = len(a)
-# print 'EX11:', 'String a is', len_a, 'characters long\nindices: ', 0, '-', len_a - 1
+# print 'EX13:', 'String a is', len_a, 'characters long\nindices: ', 0, '-', len_a - 1
 
-# Exercise 12: String formatting
+# Exercise 14: String formatting
 a = 'The inserted string is: {}. It\'s length is: {}.'
 inserted = 'LabView Sucks!'
-# print 'EX12:', a.format(inserted, len(inserted))
+# print 'EX14:', a.format(inserted, len(inserted))
 
 
 
@@ -93,22 +97,22 @@ inserted = 'LabView Sucks!'
 # List
 ####
 
-# Exercise 13: Lists and list addition
+# Exercise 15: Lists and list addition
 l1 = [1, 2, 3]
 l2 = ['1', '2', '3']
 l3 = l1 + l2
-# print 'EX13:', l3, l3[1], l3[4:6]
+# print 'EX15:', l3, l3[1], l3[4:6]
 
-# Exercise 14: Appending elements to a list, length of a list
+# Exercise 16: Appending elements to a list, length of a list
 len_before = len(l3)
 l3.append('a')
 l3.append(2.2222)
 len_after = len(l3)
-# print 'EX 14:', len_before, len_after, l3
+# print 'EX 16:', len_before, len_after, l3
 
-# Exercise 15: Removing elements of a list
+# Exercise 17: Removing elements of a list
 item_at_ind_3 = l3.pop(3)
-# print 'EX 15:', item_at_ind_3, l3[3], l3
+# print 'EX 17:', item_at_ind_3, l3[3], l3
 
 
 
@@ -116,18 +120,18 @@ item_at_ind_3 = l3.pop(3)
 # For loops
 ####
 
-# Exercise 16: Looping over a list
+# Exercise 18: Looping over a list
 a = [0, 1, 2]
 b = 0
 for i in a:
 	b += i
-# print 'EX16:', b
+# print 'EX18:', b
 
-# Exercise 17: The range() function
+# Exercise 19: The range() function
 l = []
 for i in range(10):
 	l.append(i)
-# print 'EX17', l
+# print 'EX19', l
 
 
 
@@ -135,15 +139,15 @@ for i in range(10):
 # if statement
 ####
 
-# Exercise 18: A simple if statement
+# Exercise 20: A simple if statement
 l = [0, 1, 2, 3, 4]
 if range(5) == l:
 	success = True
 else:
 	success = False
-# print 'EX18:', success
+# print 'EX20:', success
 
-# Exercise 19: If not only works with booleans!
+# Exercise 21: If not only works with booleans!
 l = []
 num = 0
 s = ''
@@ -155,13 +159,39 @@ if not l:
 		if not num2 or not num3:
 			if True:
 				pass
-				# print 'EX19: Works...'
+				# print 'EX21: Works...'
 
-# Exercise 20: the xrange() function and the break statement
+# Exercise 22: the xrange() function and the break statement
 for i in xrange(1000000000):
 	if i >= 9999:
 		break
-# print 'EX20:', i
+# print 'EX22:', i
+
+
+
+####
+# Combining what we have learned so far...
+####
+
+# Given a list with n items, how can we split this list into m separate lists?
+# The result will be a list of lists
+
+original = range(12)
+num_sub_lists = 3
+final = []
+
+for i in range(num_sub_lists):
+	final.append([])
+
+# print 'Before:', final
+
+for i in range(len(original)):
+	sub_list = i % num_sub_lists
+	element = original[i]
+	final[sub_list].append(element)
+	# print 'xxxxxxxxxxxxxxxxxxxxxxxx\nindex: {}\nlist index: {}\nelement: {}'.format(i, sub_list, element)
+
+# print 'Final:', final
 
 
 
