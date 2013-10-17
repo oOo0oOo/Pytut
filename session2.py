0893995a4d9d67010329fe4c5eb5114d4aa2d4e0
Oliver Dressler <oliver.dressler@chem.ethz.ch>
2013-10-17 15:28:04 +0200

    Added session 2 topics

---
+++ session2.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------
+#
+# Python Tutorial Session 2
+# (c) Oliver Dressler, 2013
+#
+#-------------------------------------------------
+
+
+####
+# Dictionaries
+####
+
+####
+# Unpacking values
+####
+
+#### 
+# Prompting users
+####
+
+####
+# Functions
+####
+
+####
+# While loops
+####
