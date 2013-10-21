2b76b6c0ac519e4c21fc4b9c297fe7ebdeb5393d
Oliver Dressler <odressler@gmail.com>
2013-10-21 18:45:06 +0200

    Moved problem out of session 1

---
+++ exercises/problems1.py
@@ -0,0 +1,29 @@
+####
+# Simple problems to solve
+####
+
+
+
+####
+# PROBLEM 1_A (easier):
+####
+
+# https://projecteuler.net/problem=1
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ 
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+####
+# PROBLEM 1_B:
+####
+
+# https://projecteuler.net/problem=5 (variation)
+
+# 2520 is the smallest number that can be divided by each of the numbers 
+# from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible 
+# by all of the numbers from 1 to 16?
--- session1.py
+++ session1.py
@@ -247,29 +247,3 @@ for i in range(len(original)):
 	# print 'index: {}\nlist index: {}\nelement: {}\n{}\n'.format(i, sub_list, element, '_._' * 16)
 
 # print 'Final:', final
-
-
-
-####
-# PROBLEM 1_A (easier):
-####
-
-# https://projecteuler.net/problem=1
-
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, 
-# we get 3, 5, 6 and 9. The sum of these multiples is 23.
- 
-# Find the sum of all the multiples of 3 or 5 below 1000.
-
-
-####
-# PROBLEM 1_B:
-####
-
-# https://projecteuler.net/problem=5 (variation)
-
-# 2520 is the smallest number that can be divided by each of the numbers 
-# from 1 to 10 without any remainder.
-
-# What is the smallest positive number that is evenly divisible 
-# by all of the numbers from 1 to 16?
