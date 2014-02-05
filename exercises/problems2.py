475fedfd28dd086bee1d68cf0b6b8448d0711d14
Oliver Dressler <oliver.dressler@chem.ethz.ch>
2014-02-05 11:09:03 +0100

    Changed problems 2

--- exercises/problems2.py
+++ exercises/problems2.py
@@ -10,12 +10,14 @@
 # Your code can not be longer than one line (Hint: use a list comprehension in there).
 
 
+
 ####
 # PROBLEM 2_B
 ####
 
-## Functions on steroids
-
+## Write a function translate() that will translate a text into "rövarspråket" (Swedish for "robber's language" / pig latin). 
+## That is, double every consonant and place an occurrence of 'o' in between. 
+## For example, translate('this is fun') should return the string 'tothohisos isos fofunon'.
 
 
 
